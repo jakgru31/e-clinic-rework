e5ad310512fa4ae2625bae24c854ffd58a94c0e9
Jakub Gruszka <jakub226@outlook.com>
2026-09-20 23:35:43 +0200

    Updated Firestore rules regarding access to medical_records

--- app/src/main/java/com/example/e_clinic/UI/activities/doctor_screens/doctor_activity/PrescribeScreen.kt
+++ app/src/main/java/com/example/e_clinic/UI/activities/doctor_screens/doctor_activity/PrescribeScreen.kt
@@ -7,12 +7,9 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
@@ -428,6 +425,7 @@ fun PrescribeScreen(
                         LaunchedEffect(patientId) {
                             db.collection("medical_records")
                                 .whereEqualTo("user_id", patientId)
+                                .whereEqualTo("doctor_id", id)
                                 .limit(1)
                                 .get()
                                 .addOnSuccessListener { result ->
